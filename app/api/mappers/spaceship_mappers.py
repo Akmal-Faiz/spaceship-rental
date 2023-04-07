89e389ee8a1527314311f3f606798785398801b3
akmal <abinomar@dxc.com>
2023-04-08 01:22:26 +0800

    changed response model

--- app/api/mappers/spaceship_mappers.py
+++ app/api/mappers/spaceship_mappers.py
@@ -1,5 +1,5 @@
 def toResponse(result):
-    return [contract["name"] for contract in result[0]]
+    return {"income": result[1], "path":[c["name"] for c in result[0]]}
 
 def toContracts(contractList):
     return [c.__dict__ for c in contractList.__root__]
